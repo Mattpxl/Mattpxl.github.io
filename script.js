ed381a9c399e8f261076bbc6e5bde0efbd7c08c7
Mattpxl <Mdppxl@gmail.com>
2024-10-21 16:17:50 -0400

    contact

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Function to open an email form
 function openEmail() {
-    window.location.href = "mailto:example@example.com?subject=Hello&body=I%20am%20interested%20in%20your%20services.";
+    window.location.href = "mailto:mdppxl@gmail.com?subject=Hello&body=I%20am%20interested%20in%20your%20services.";
 }
 
 // Event listener to call openEmail when button is clicked
